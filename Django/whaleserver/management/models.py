293c15b1d2d49fab83d8ef4174a1b8a1213dfd63
Barbara Flindt <barbaraalbertine@gmail.com>
2011-09-22 10:52:00 +0200

    Track model is done

--- Django/whaleserver/management/models.py
+++ Django/whaleserver/management/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Track(models.Model):
+	title = models.CharField(max_length=30)
+	artist = models.CharField(max_length=30)
+	album = models.CharField(max_length=30)
+	year = models.CharField(max_length=4)
+	comment = models.CharField(max_length=30)
+	genre = models.IntegerField()
+	trackNo = models.IntegerField()
+	duration = models.IntegerField()
+	path = models.CharField(max_length=200)
+	hashNo = models.CharField(max_length=33)
+	
