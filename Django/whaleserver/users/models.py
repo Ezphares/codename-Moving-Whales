be254e1f97057c4ad53a931befd0bd1350fca06c
Anders Vinther <anders.vinther1@gmail.com>
2011-09-28 21:01:37 +0200

    I hope this added the users app for vandborg <3

--- Django/whaleserver/users/models.py
+++ Django/whaleserver/users/models.py
@@ -4,3 +4,20 @@ from django.db import models
 
 class User(models.Model):
 	username = models.CharField(max_length = 200)
+	password = models.CharField(max_length = 200)
+	firstname = models.CharField(max_length = 200)
+	lastname = models.CharField(max_length = 200)
+	emailaddress = models.EmailField(max_length = 200)
+	created = models.DateTimeField(auto_now_add=True)
+	modified = models.DateTimeField(auto_now=True)
+	lastlogin = models.DateTimeField(auto_now_add=True)
+	country = models.CharField(max_length = 200)
+	birthday = models.DateField()
+	biography = models.TextField()
+	rights = models.IntegerField(max_length = 2)
+
+class User__User(models.Model):
+	user = models.ForeignKey(User, related_name='user')
+	friend = models.ForeignKey(User, related_name='friend')
+	type = models.IntegerField(max_length = 2) # 0 = request, 1 = friend, 2 = ignored
+	created = models.DateTimeField(auto_now_add=True)
--- Django/whaleserver/users/views.py
+++ Django/whaleserver/users/views.py
@@ -1,1 +1,27 @@
 # Create your views here.
+from django.http import HttpResponse
+from users.models import User, User__User
+import json
+
+def profile(request, user_id):
+	user = User.objects.get(pk=user_id)
+	obj = {
+		'meta': {
+			'errors': [],
+			'type': 'profile'
+		},
+		'data': {
+			'username': user.username,
+			'firstname': user.firstname,
+			'lastname': user.lastname,
+			'emailaddress': user.emailaddress,
+			'created': user.created.strftime("%d/%m-%Y %H:%M"),
+			'modified': user.modified.strftime("%d/%m-%Y %H:%M"),
+			'lastlogin': user.lastlogin.strftime("%d/%m-%Y %H:%M"),
+			'country': user.country,
+			'birthday': user.birthday.strftime("%d/%m-%Y"),
+			'biography': user.biography,
+			'rights': user.rights,
+		}
+	}
+	return HttpResponse(json.dumps(obj))
