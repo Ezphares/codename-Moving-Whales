6d328ac93bdaffcf3d353bd7b2edc9b30eb9f4ab
Henrik Tudborg <henrik@tudb.org>
2011-10-12 15:51:19 +0200

    adding frontend part to urls.py

--- Django/whaleserver/urls.py
+++ Django/whaleserver/urls.py
@@ -1,12 +1,20 @@
 from django.conf.urls.defaults import patterns, include, url
-
+from django.conf import settings
+from django.views.generic.simple import direct_to_template
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+
+	(r'^$', direct_to_template, {'template':'whales_index.html'}),
+
 	(r'^management/', include('management.urls')),
 	(r'^users/', include('users.urls')),
+
+	(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        	{'document_root': settings.STATIC_DOC_ROOT}),
+
     # Examples:
     # url(r'^$', 'whaleserver.views.home', name='home'),
     # url(r'^whaleserver/', include('whaleserver.foo.urls')),
