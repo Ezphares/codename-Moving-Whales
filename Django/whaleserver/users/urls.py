b9a3281b6e77a746f59e4f7e4989341efdb1a83d
Jeppe Tarp <raider.j@gmail.com>
2011-10-26 10:58:45 +0200

    Added url pattern to handle profile searches

--- Django/whaleserver/users/urls.py
+++ Django/whaleserver/users/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns ('',
 	(r'^profile/$', 'users.views.profile',{"user_id":None}),
 	(r'^profile/(\d+)/$', 'users.views.profile'),
+	(r'^profile/search/', 'users.views.search'),
 	(r'^login/$', 'users.views.login_view'),
 	(r'^logout/$', 'users.views.logout_view'),
 	(r'^register/$', 'users.views.register'),
