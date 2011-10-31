cddcc6a1d483c5d438972cd205d8bbe920ecaa6d
Jeppe Tarp <raider.j@gmail.com>
2011-10-31 11:50:39 +0100

    Javascript for viewing profiles and their friends

--- Django/whaleserver/media/js/whales.community.js
+++ Django/whaleserver/media/js/whales.community.js
@@ -6,6 +6,7 @@ whales.community = {};
 
 whales.community.viewprofile = function(id)
 {
+	var url = '';
 	if (typeof id === 'undefined')
 	{
 		url = 'users/profile/';
@@ -19,6 +20,36 @@ whales.community.viewprofile = function(id)
 		if (data.meta.errors.length === 0)
 		{
 			$('#community_content').html(tEngine.apply(data.data.profile, templates.template_profilepage));
+			
+			var friendstring = '<br/>Friends:<br/>';
+			var i = 0;
+			for (i = 0; i < data.data.friends.length; i++)
+			{
+				friendstring += tEngine.apply(data.data.friends[i], templates.template_user);
+			}
+			$('#profilepage_friends').html(friendstring);
+			
+			if (data.data.requests.length === 0)
+			{
+				$('#button_accept_requests').hide();
+			}
+			else
+			{
+				var plural = '';
+				if (data.data.requests.length > 1)
+				{
+					plural = 's';
+				}
+				$('#button_accept_requests').html(data.data.requests.length + ' friend request' + plural);
+				$('#button_accept_requests').data('requests', data.data.requests);
+			}
+			
+			if (data.data.profile.relation !== 'stranger_add')
+			{
+				$('#button_add_friend').hide();
+			}
 		}
 	});
+	
+	// TODO: Move community events here.
 }
