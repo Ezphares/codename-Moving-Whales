9a0c5c7724b1185c305736f33363ba3decd1eeab
Jeppe Tarp <raider.j@gmail.com>
2011-11-14 13:00:23 +0100

    Restored urls.py, silly anders

---
+++ Django/whaleserver/management/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+	(r'^upload/$', 'management.views.upload'),
+	(r'^sendfile/$', 'management.views.sendfile'),
+	(r'^library/$', 'management.views.get_library'),
+	(r'^getplaylists/$', 'management.views.getplaylists'),
+	(r'^createplaylist/$', 'management.views.createplaylist'),
+	(r'^deleteplaylist/$', 'management.views.deleteplaylist'),
+	(r'^addsongtoplaylist/$', 'management.views.addsongtoplaylist'),
+	(r'^deletesongtoplaylist/$', 'management.views.deletesongtoplaylist'),
+)
