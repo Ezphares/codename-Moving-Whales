b5ab1ad931cbd519132c5f59bb041783ac48dbf5
Henrik Tudborg <henrik@tudb.org>
2011-11-19 13:12:38 +0100

    Trying to make player work.
    Now the player is working on our own tracks, but due to changes in the backend, all CURRENT tracks will be corrupt. Delete all tracks from tracks folder, delete database file, syncdb and try again.
    
    You can now drag drags from library and drop on player to play them.

--- Django/whaleserver/media/js/whales.dragndrop.js
+++ Django/whaleserver/media/js/whales.dragndrop.js
@@ -10,20 +10,31 @@ whales.dragndrop.init = function(){
 		$('#library_list > .track').attr("draggable", "true").bind("dragstart", function(event){
 			var dt = event.originalEvent.dataTransfer;
 			dt.setData("Text", $(this).data('link_id').toString());
-			whales.playlist.flyout(true);
+			//whales.playlist.flyout(true);
 			return true;
 		}).bind("dragend", function(event){
-			whales.playlist.flyout(false);
+			//whales.playlist.flyout(false);
 			return false;
 		});
 	});
-	
+
 	$('#sidebar_playlist > .sidebar_pane').bind("drop", function(event){
 		var dt = event.originalEvent.dataTransfer;
 		console.log(dt);
 		var msg = SyncObjects.get("addtrack");
 		msg.payload = {type: 'addtrack', link_id: dt.getData('Text')};
 		whales.sync.send(msg);
+		return false;
+	}).bind("dragenter dragover dragleave", function(event){
+		return false;
+	});
+
+	$('#player_wrapper').bind("drop", function(event){
+		var dt = event.originalEvent.dataTransfer;
+		whales.player.stop();
+                var path = $(".track[data-link_id="+dt.getData('Text')+"]").data("path");
+                whales.player.load(path);
+                whales.player.play();
 		return false;
 	}).bind("dragenter dragover dragleave", function(event){
 		return false;
